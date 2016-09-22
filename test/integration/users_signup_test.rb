4e73310afc8bdcf0d5a76b2c018dfa55c121569f
cupatea <vdavyden@gmail.com>
2016-09-22 23:13:24 +0300

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+    post users_path, params: { user: { name:  "",
                                        email: "user@invalid",
                                        password:              "foo",
                                        password_confirmation: "bar" } }
@@ -15,13 +19,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "Example User ",
+    post users_path, params: { user: { name:  "Example User ",
                                          email: "user@example.com",
                                          password:              "password",
                                          password_confirmation: "password" } }
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash[:success]==nil
+    assert is_logged_in?
+    assert_not flash.empty?
   end
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    end
+
 end
