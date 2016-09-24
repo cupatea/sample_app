2628634cc0d20d6ad26d340503b0f85353666def
cupatea <vdavyden@gmail.com>
2016-09-24 22:24:01 +0300

    layout_test_problem

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user       = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_page/home'
@@ -12,5 +17,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    get login_path
+    assert_select "title", full_title("Log in")
+    log_in_as (@user)
+    get users_path
+    assert_select "title", full_title("All users")
+    #To be contunie
+
   end
+
 end
