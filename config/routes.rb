ca2e111b88cacf789daaacb5a462069b961d59d7
cupatea <vdavyden@gmail.com>
2016-10-04 22:51:25 +0300

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  resources :users
+  root                'static_page#home'
+  get    'help'    => 'static_page#help'
+  get    'about'   => 'static_page#about'
+  get    'contact' => 'static_page#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
-
-  root   'static_page#home'
-  get    '/help',    to: 'static_page#help'
-  get    '/about',   to: 'static_page#about'
-  get    '/contact', to: 'static_page#contact'
-  get    '/signup',  to: 'users#new'
-  post   '/signup',  to: 'users#create'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-
+  resources :relationships,       only: [:create, :destroy]
 end
